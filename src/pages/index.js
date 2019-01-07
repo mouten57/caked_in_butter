020f86787fcbc0c96c0d96a4d3b179261aa57ab1
Matt Outen <mattoutenphysed@gmail.com>
2019-01-07 14:41:17 -0600

    added pic, link, changed title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    Coming soon...<a href="https://www.cakedinbutter.com">CakedInButter.com</a>
+    <hr />
+    <img
+      src="https://cdn.pixabay.com/photo/2016/12/11/08/01/coming-soon-1898936_960_720.jpg"
+      alt=""
+    />
+  </div>
+);
