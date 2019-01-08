76a7ad1b78847a96bf3d89a05c84151b1fd07936
Matt Outen <mattoutenphysed@gmail.com>
2019-01-08 13:04:44 -0600

    remove placeholder index image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
+
 import Layout from '../components/layout';
 
-export default ({ data }) => {
+export default () => {
   return (
     <Layout>
       <div style={{ textAlign: 'center' }}>
         Coming soon...
         <a href="https://www.cakedinbutter.com">CakedInButter.com</a>
         <hr />
-        <div style={{ margin: '0 auto' }}>
-          <Img
-            style={{ margin: '0 auto', display: '' }}
-            fixed={data.file.childImageSharp.fixed}
-          />
-        </div>
+        <div style={{ margin: '0 auto' }} />
         <p style={{ textAlign: 'center' }}>
           For now, stay in touch on{' '}
           <a href="https://www.twitter.com/cakedinbutter">Twitter</a> and{' '}
@@ -25,17 +19,3 @@ export default ({ data }) => {
     </Layout>
   );
 };
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "coming-soon.jpg" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fixed(width: 300) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`;
