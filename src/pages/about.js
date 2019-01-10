e8a49a56f0dde4483137261f6b29aff5164d4b71
Matt Outen <mattoutenphysed@gmail.com>
2019-01-10 06:18:09 -0600

    about me 1st draft

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,22 +14,30 @@ export default props => {
       <h1 className="page-title">About</h1>
 
       <p />
-      <p>Hello!</p>
+
       <p>
-        This is just a placeholder line. This is just a placeholder line. This
-        is just a placeholder line. This is just a placeholder line. This is
-        just a placeholder line.{' '}
+        Hi friends! I’m Emily Outen, a Carolina girl living in Sugar Land, Texas
+        with my husband Matt and our newest addition: our daughter, Monroe
+        Grace. In addition to loving all things buttery and sweet, I enjoy hot
+        coffee, cookware, cuddles with Monroe, and sushi dates with Matt.
       </p>
       <p>
-        Hello! This is just a placeholder line. This is just a placeholder line.
-        This is just a placeholder line. This is just a placeholder line. This
-        is just a placeholder line.{' '}
+        I was introduced to cooking at a young age when I would help my Mom in
+        the kitchen while she cooked for our boisterous family of 7, but my love
+        and passion for baking started fairly recently during my pregnancy with
+        Monroe. Though I enjoyed cooking, I was always afraid of baking because
+        of the precision it required. Now, it doesn’t scare me so much. I love
+        seeing my bakes when they’re complete and even more than that I love
+        sharing them with the people I love.
       </p>
       <p>
-        Hello! This is just a placeholder line. This is just a placeholder line.
-        This is just a placeholder line. This is just a placeholder line. This
-        is just a placeholder line.{' '}
+        Food has always meant ‘togetherness’ to me- so sharing my bakes with
+        family and friends brings me such a great joy! My hope for this blog is
+        that you are inspired to get your kitchen a little bit messy, get some
+        flour in your hair and bring your loved ones together around something
+        buttery and crumbly.
       </p>
+      <p>Bon appetit!</p>
     </PageTemplate>
   );
 };
