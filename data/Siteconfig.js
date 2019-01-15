bbfd65751ed58b2b4360b5326b7ed8df4085bef6
Matt Outen <mattoutenphysed@gmail.com>
2019-01-14 19:56:32 -0600

    updated google analytics to emilys google account

--- data/Siteconfig.js
+++ data/Siteconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   author: 'Matt Outen',
   description: 'Caked in butter bakery',
   copyright: 'Copyright © 2019. Matt Outen',
-  googleAnalyticsID: 'UA-131833683-1',
+  googleAnalyticsID: 'UA-132422946-1',
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
   backgroundColor: '#e0e0e0' // Used for setting manifest background color.
 };
